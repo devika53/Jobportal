d10dc30cc10382d78f0cdc9d014b60f6591bf533
devika53 <59084285+devika53@users.noreply.github.com>
2023-04-04 18:47:10 +0530

    Add files via upload

---
+++ broad/src/main/java/com/job/broad/config/SecurityConfig.java
@@ -0,0 +1,134 @@
+package com.job.broad.config;
+
+import com.job.broad.filters.JWTAuthorizationFilter;
+import com.job.broad.filters.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import static org.springframework.http.HttpMethod.*;
+import static org.springframework.http.HttpMethod.POST;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private final UserDetailsService userDetailsService;
+    @Autowired
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public SecurityConfig(UserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        JwtAuthenticationFilter customAuthenticationFilter = new JwtAuthenticationFilter(authenticationManagerBean());
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login/**");
+        http.cors();
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.authorizeRequests().antMatchers(
+                "/api/login/**",
+                "/api/login",
+                "/api/welcome",
+                "/api/v1/addUser",
+                "/api/roles/**",
+                "/api/role/addtouser/**",
+                "/api/token/refresh/**",
+                "/api/users/save/**",
+                "/swagger-ui.html",
+                "/swagger-resources/**",
+                "/v2/api-docs",
+                "/configuration/ui",
+                "/webjars/**").permitAll();
+        /*
+          Listing all function having access to user with role ADMIN,APPLICANT,EMPLOYER
+        */
+
+        http.authorizeRequests().antMatchers(GET, "/api/users/**")
+                .hasAnyAuthority("ADMIN");
+        http.authorizeRequests().antMatchers(POST, "/api/role/save/**")
+                .hasAnyAuthority("ADMIN");
+        http.authorizeRequests().antMatchers(GET, "/api/role/**")
+                .hasAnyAuthority("ADMIN");
+        http.authorizeRequests().antMatchers(PUT, "/api/role/update/**")
+                .hasAnyAuthority("ADMIN");
+        http.authorizeRequests().antMatchers(DELETE, "/api/role/delete/**")
+                .hasAnyAuthority("ADMIN");
+        http.authorizeRequests().antMatchers(POST, "/api/tags/save/**")
+                .hasAnyAuthority("ADMIN");
+        http.authorizeRequests().antMatchers(GET, "/api/tags/**")
+                .hasAnyAuthority("ADMIN", "EMPLOYER", "APPLICANT");
+        http.authorizeRequests().antMatchers(PUT, "/api/tags/update/**")
+                .hasAnyAuthority("ADMIN");
+        http.authorizeRequests().antMatchers(DELETE, "/api/tags/delete/**")
+                .hasAnyAuthority("ADMIN");
+        http.authorizeRequests().antMatchers(GET, "/api/jobs/**")
+                .hasAnyAuthority("EMPLOYER", "ADMIN");
+        http.authorizeRequests().antMatchers(POST, "/api/job/save/**")
+                .hasAnyAuthority("EMPLOYER");
+        http.authorizeRequests().antMatchers(POST, "/api/job/create/**")
+                .hasAnyAuthority("EMPLOYER");
+        http.authorizeRequests().antMatchers(PUT, "/api/job/update/{jobId}/**")
+                .hasAnyAuthority("EMPLOYER");
+        http.authorizeRequests().antMatchers(POST, "/api/job/find/**")
+                .hasAnyAuthority("EMPLOYER");
+        http.authorizeRequests().antMatchers(POST, "/api/job/addskills/**")
+                .hasAnyAuthority("EMPLOYER");
+        http.authorizeRequests().antMatchers(GET, "/api/applicant/**")
+                .hasAnyAuthority("APPLICANT").and().csrf().disable();
+        http.authorizeRequests().antMatchers(POST, "/api/skill/addtouser/**")
+                .hasAnyAuthority("APPLICANT");
+        http.authorizeRequests().antMatchers(POST, "/api/job/apply/**")
+                .hasAnyAuthority("APPLICANT");
+        http.authorizeRequests().antMatchers(POST, "/api/jobs/applied/**")
+                .hasAnyAuthority("EMPLOYER");
+        http.authorizeRequests().antMatchers(GET, "/api/users/**")
+                .hasAnyAuthority("EMPLOYER", "APPLICANT");
+        http.authorizeRequests().antMatchers(POST, "/api/job/hire/{jobId}/{email}/**")
+                .hasAnyAuthority("EMPLOYER");
+        http.authorizeRequests().anyRequest().authenticated();
+        http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+    }
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("PUT", "DELETE", "POST", "GET");
+            }
+        };
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
